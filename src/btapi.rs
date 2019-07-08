e84cf1b188c6f859ae6c3bfa5aa2632ce9ffeab1
Attenuation <ouyangjun1999@gmail.com>
2019-07-09 00:17:19 +0800

    fix btapi

--- src/btapi.rs
+++ src/btapi.rs
@@ -5,6 +5,7 @@ use hyper::{header, Body, Chunk, Method, StatusCode, Uri};
 use hyper::{Client, Request, Response, Server};
 use serde_json;
 use std::net::SocketAddr;
+use log::*;
 
 use serde::{Serialize, Deserialize};
 use crate::http;
@@ -17,21 +18,50 @@ static AE: &str = "gzip";
 type GenericError = Box<dyn std::error::Error + Send + Sync>;
 type ResponseFuture = Box<dyn Future<Item = Response<Body>, Error = GenericError> + Send>;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug,Clone)]
 struct Btdata {
     r#as : String,
     area: String
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Ipdata {
+    as_num: String,
+    l1: String,
+    l2: String,
+    l3: String,
+    l4: String,
+    isp: String,
+    lat: String,
+    lng: String
+}
+
+impl Ipdata {
+    fn new(bt: Btdata) -> Ipdata {
+        let as_num = bt.r#as;
+        let area: Vec<&str> = bt.area.split("\t").collect();
+        Ipdata {
+            as_num: as_num,
+            l1: area[0].to_string(),
+            l2: area[1].to_string(),
+            l3: area[2].to_string(),
+            l4: area[3].to_string(),
+            isp: area[4].to_string(),
+            lat: area[5].to_string(),
+            lng: area[6].to_string(),
+        }
+    }
+}
+
 pub fn bt_api(req: Request<Body>, remote_addr: SocketAddr) -> ResponseFuture {
     let ip  = http::Ipfromrequerst(req, &remote_addr).unwrap();
-    dbg!(&ip);
+    // dbg!(&ip);
     let url = format!(
         "http://btapi.ipip.net/host/info?ip={}&host=&lang={}",
         &ip, "cn"
     );
     let bt_url = url.parse::<Uri>().unwrap();
-    dbg!(&bt_url);
+    // dbg!(&bt_url);
     let req = Request::builder()
         .method(Method::GET)
         .uri(bt_url)
@@ -44,12 +74,14 @@ pub fn bt_api(req: Request<Body>, remote_addr: SocketAddr) -> ResponseFuture {
             .request(req)
             .from_err()
             .map( |web_res| {
-                dbg!(&web_res);
-                let body = Body::wrap_stream(web_res.into_body().map(|b| {
+                let body = Body::wrap_stream(web_res.into_body().map(move |b| {
                     let data: Btdata = serde_json::from_slice(&b).unwrap();
+                    let ip_data = Ipdata::new(data);
+                    let address = ip_data.l1 + " " + &ip_data.l2 + " " + &ip_data.l3;
+                    info!("ip: {},address: {}",ip,address);
                     Chunk::from(format!(
-                        "AS号码 : {}\n地址   ：{}\n",
-                        data.r#as,data.area
+                        "IP     : {}\nAS号码 : {}\n地址   ：{}\n运营商 : {}\n",
+                        ip,ip_data.as_num,address,ip_data.isp
                     ))
                 }));
                 Response::new(body)
